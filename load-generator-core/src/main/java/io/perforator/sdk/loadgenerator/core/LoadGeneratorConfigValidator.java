862642f6586fa5fa63deb092f4d68812139ac828
Max Artyukhov <max@perforator.io>
2022-05-24 21:12:26 -0500

    Adjusting browserCloudHttpHeaders validation

--- load-generator-core/src/main/java/io/perforator/sdk/loadgenerator/core/LoadGeneratorConfigValidator.java
+++ load-generator-core/src/main/java/io/perforator/sdk/loadgenerator/core/LoadGeneratorConfigValidator.java
@@ -19,33 +19,37 @@ final class LoadGeneratorConfigValidator {
     
     private static final Pattern HOST_NAME_PATTERN = Pattern.compile("^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$");
     private static final Pattern IP_V4_PATTERN = Pattern.compile("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}");
+    private static final Pattern HEADER_NAME_PATTERN = Pattern.compile("[^\\s:]+");
 
     private LoadGeneratorConfigValidator() {
     }
 
     public static LoadGeneratorConfig validate(LoadGeneratorConfig loadGeneratorConfig) {
         if (loadGeneratorConfig.getBrowserCloudHttpHeaders() != null && !loadGeneratorConfig.getBrowserCloudHttpHeaders().isEmpty()) {
-
-            Pattern namePattern = Pattern.compile("^([\\w-]+)$");
-            Pattern valuePattern = Pattern.compile("^([^;]+)$");
-
             for (Map.Entry<String, String> entry : loadGeneratorConfig.getBrowserCloudHttpHeaders().entrySet()) {
                 String name = entry.getKey();
                 String value = entry.getValue();
+                
+                if (name == null || name.isBlank()) {
+                    throw new RuntimeException(
+                            "Bad '"
+                            + LoadGeneratorConfig.Fields.browserCloudHttpHeaders
+                            + "' field format - header name is required"
+                    );
+                }
 
-                if (name == null || !namePattern.matcher(name).find()) {
+                if (!HEADER_NAME_PATTERN.matcher(name).matches()) {
                     throw new RuntimeException(
                             "Bad '" + LoadGeneratorConfig.Fields.browserCloudHttpHeaders + "' field format." +
                                     " The header's name '" + name + "' is invalid."
                     );
                 }
-                if (value == null || !valuePattern.matcher(value).find()) {
+                if (value == null) {
                     throw new RuntimeException(
-                            "Bad '" + LoadGeneratorConfig.Fields.browserCloudHttpHeaders + "' field format. " +
-                                    "The header's value '" + value + "' is invalid."
+                            "Bad '" + LoadGeneratorConfig.Fields.browserCloudHttpHeaders + "' field format. "
+                            + "Header '" + name + "' value is required"
                     );
                 }
-
             }
         }
 
